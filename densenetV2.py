088b115b8dd37c99d80c361034c41eb25a24fe51
nengwenpeng <44516353+nengwenpeng@users.noreply.github.com>
2020-05-31 20:37:31 +0800

    Add files via upload

---
+++ densenetV2.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+
+
+def Bottleneck(x,growthRate,kernel_size):
+    interChannels = 4 * growthRate
+    network = tf.layers.batch_normalization(inputs=x)
+    network = tf.nn.relu(network)
+    network = tf.layers.conv1d(inputs=network, filters=interChannels, kernel_size=1, strides=1,
+                               padding='same', activation=None, use_bias=False)
+    network = tf.layers.batch_normalization(inputs=network)
+    network = tf.nn.relu(network)
+    network = tf.layers.conv1d(inputs=network, filters=growthRate, kernel_size=kernel_size, strides=1,
+                               padding='same', activation=None, use_bias=False)
+    network = tf.concat((x, network), 2)
+    return network
+
+
+def Pool_block(x,out_cha,keep_prob_=0.8):
+    network = tf.layers.batch_normalization(inputs=x)
+    network = tf.nn.relu(network)
+    network = tf.layers.conv1d(inputs=network, filters=out_cha, kernel_size=1, strides=1,
+                               padding='same', activation=tf.nn.relu, use_bias=False)
+    network = tf.layers.average_pooling1d(inputs=network, pool_size=2, strides=2, padding='same')
+    network = tf.nn.dropout(network, keep_prob_)
+    return network
+
+def head_cnn(x,keep_prob_=0.8):
+    network = tf.layers.conv1d(inputs=x, filters=64, kernel_size=50, strides=6,
+                               padding='same', activation=None, use_bias=False)
+    network = tf.layers.batch_normalization(inputs=network)
+    network = tf.nn.relu(network)
+    # 500
+    network = tf.layers.max_pooling1d(inputs=network, pool_size=8, strides=8, padding='same')
+    network = tf.nn.dropout(network, keep_prob_)
+    # 64
+    return network
+
+def Dense_block(x,in_cha,growthRate=12,kernel_size=3):
+    network = Bottleneck(x, growthRate, kernel_size)
+    network = Bottleneck(network, growthRate*1+in_cha, kernel_size)
+    network = Bottleneck(network, growthRate*2+in_cha, kernel_size)
+    network = Bottleneck(network, growthRate*3+in_cha, kernel_size)
+    return network
+
+
+def Dense_net(x,growthRate=24,kernel_size=3,keep_prob_=0.5):
+    network = head_cnn(x, keep_prob_)
+    # 64
+    in_cha = 128
+    network = Dense_block(network, in_cha, growthRate, kernel_size)
+    out_cha = 128
+    network = Pool_block(network,out_cha, keep_prob_)
+    # 32
+    in_cha = out_cha
+    network = Dense_block(network, in_cha, growthRate, kernel_size)
+    out_cha = 256
+    network = Pool_block(network,out_cha,keep_prob_)
+    # 16
+    in_cha = out_cha
+    network = Dense_block(network, in_cha, growthRate, kernel_size)
+    out_cha = 512
+    network = Pool_block(network,out_cha,keep_prob_)
+    # 8
+    network = Dense_block(network, in_cha, growthRate, kernel_size)
+
+    network = tf.layers.average_pooling1d(inputs=network, pool_size=8, strides=8, padding='same')
+
+    return network
---
+++ densenet_yuanshi.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+
+
+def flatten(name, input_var):
+    # 对数据进行 压扁暂时不知道输入维度形式，把卷积以后的 压缩成向量
+    dim = 1
+    for d in input_var.get_shape()[1:].as_list():
+        dim *= d
+    output_var = tf.reshape(input_var,
+                            shape=[-1, dim],
+                            name=name)
+
+    return output_var
+
+
+def Dense_net(x,growthRate=24,kernel_size=3,keep_prob_=0.5):
+    # List to store the output of each CNNs
+    output_conns = []
+    ######### CNNs with small filter size at the first layer #########
+
+    # Convolution   y = f(x) 这种写法，特别相似,比较像直接写前向传递函数，而不是先定义
+    network = tf.layers.conv1d(inputs=x, filters=64, kernel_size=50, strides=6,
+                               padding='same', activation=tf.nn.relu)
+    # network = tf.layers.conv1d(inputs=input_var, filters=64, kernel_size=50, strides=6,
+
+    network = tf.layers.max_pooling1d(inputs=network, pool_size=8, strides=8, padding='same')
+
+    # Dropout
+    network = tf.nn.dropout(network, keep_prob_)
+
+    # Convolution
+    network = tf.layers.conv1d(inputs=network, filters=128, kernel_size=8, strides=1,
+                               padding='same', activation=tf.nn.relu)
+
+    network = tf.layers.conv1d(inputs=network, filters=128, kernel_size=8, strides=1,
+                               padding='same', activation=tf.nn.relu)
+    network = tf.layers.conv1d(inputs=network, filters=128, kernel_size=8, strides=1,
+                               padding='same', activation=tf.nn.relu)
+
+    # Max pooling
+    network = tf.layers.max_pooling1d(inputs=network, pool_size=4, strides=4, padding='same')
+
+    # Flatten
+    network = flatten(name="flat1", input_var=network)
+
+    output_conns.append(network)
+
+    ######### CNNs with large filter size at the first layer #########
+
+    # Convolution
+    network = tf.layers.conv1d(inputs=input_var, filters=64, kernel_size=400, strides=50,
+                               padding='same', activation=tf.nn.relu)
+
+    network = tf.layers.max_pooling1d(inputs=network, pool_size=4, strides=4, padding='same')
+
+    # Dropout
+    network = tf.nn.dropout(network, keep_prob_)
+
+    # Convolution
+    network = tf.layers.conv1d(inputs=network, filters=128, kernel_size=6, strides=1,
+                               padding='same', activation=tf.nn.relu)
+
+    network = tf.layers.conv1d(inputs=network, filters=128, kernel_size=6, strides=1,
+                               padding='same', activation=tf.nn.relu)
+    network = tf.layers.conv1d(inputs=network, filters=128, kernel_size=6, strides=1,
+                               padding='same', activation=tf.nn.relu)
+
+    # Max pooling
+    network = tf.layers.max_pooling1d(inputs=network, pool_size=2, strides=2, padding='same')
+
+    # Flatten
+    network = flatten(name="flat2", input_var=network)
+
+    output_conns.append(network)
+
+    # Concat
+    network = tf.concat(output_conns, 1, name="concat1")
+
+    # Dropout
+    network = tf.nn.dropout(network, keep_prob_)
+
+    return network
